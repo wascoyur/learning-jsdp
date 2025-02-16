b7dbb03efaa618f698991efe0a720c91388dff02
yuriy.vasilev <Yuriy.Va.Vasilev@ural.rt.ru>
2025-02-17 00:22:57 +0500

    добавлена вторая реализация Медиатора

--- src/examples/behavioral/Mediator/MediatorEmployee.tsx
+++ src/examples/behavioral/Mediator/MediatorEmployee.tsx
@@ -2,6 +2,7 @@ import s from "./MediatorEmployee.module.scss";
 import { useStore } from "../store.ts";
 import { Card } from "../../../components/Card/Card.tsx";
 import Mediator from "./EmployeeMediator";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 
 const EmployeeList = () => {
   const { store, addItem } = useStore();
@@ -20,8 +21,68 @@ const EmployeeList = () => {
           ))}
         </ul>
       </div>
+
+      <SecondMediator />
     </Card>
   );
 };
 
 export default EmployeeList;
+
+const SecondMediator = () => {
+  const createMediator = () => {
+    const subscribers: Record<
+      string,
+      Dispatch<SetStateAction<string | null>>[]
+    > = {};
+
+    return {
+      subscribe: (
+        event: string,
+        callback: Dispatch<SetStateAction<string | null>>,
+      ) => {
+        if (!subscribers[event]) {
+          subscribers[event] = [];
+        }
+        subscribers[event].push(callback);
+      },
+      publish: (event: string, data: string) => {
+        subscribers[event]?.forEach((callback) => callback(data));
+      },
+    };
+  };
+
+  const mediator = createMediator();
+
+  const TabButton = ({ index }: { index: string }) => (
+    <button
+      className={s.button}
+      onClick={() => mediator.publish("tabChange", index)}
+    >
+      Tab {index}
+    </button>
+  );
+
+  const TabContent = () => {
+    const [activeTab, setActiveTab] = useState<string | null>(null);
+
+    useEffect(() => {
+      return mediator.subscribe("tabChange", setActiveTab);
+    }, []);
+
+    return activeTab ? (
+      <div className={s.tabContent}>Выбранная вкладка: {activeTab}</div>
+    ) : (
+      <div>Выберите вкладку</div>
+    );
+  };
+
+  return (
+    <div className={s.container}>
+      <p>Second implementation</p>
+      <TabButton index={"left"} />
+      <TabButton index={"right"} />
+      <TabContent />
+    </div>
+  );
+};
