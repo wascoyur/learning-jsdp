a24a087a92b9d95c9cbcd306c161198669d1bbeb
yuriy.vasilev <Yuriy.Va.Vasilev@ural.rt.ru>
2025-02-16 18:27:13 +0500

    final паттерн Медиатор в Реакте он же - Container Component

---
+++ src/examples/behavioral/Mediator/EmployeeMediator.tsx
@@ -0,0 +1,101 @@
+import { useState } from "react";
+import { Employee, Store } from "../store.ts";
+import s from "./MediatorEmployee.module.scss";
+
+const managers = ["Alice", "Bob", "Charlie", "David", "Eve"];
+const roles = [
+  "Software Engineer",
+  "Product Manager",
+  "Data Scientist",
+  "UX Designer",
+  "QA Engineer",
+  "DevOps Engineer",
+  "Project Manager",
+  "Business Analyst",
+  "Marketing Specialist",
+  "HR Manager",
+];
+
+interface EmployeeMediatorProps {
+  addItem: <T extends { uid: string }>(
+    item: Omit<T, "uid">,
+    key: keyof Store,
+  ) => void;
+}
+
+const EmployeeMediator = ({ addItem }: EmployeeMediatorProps) => {
+  const [name, setName] = useState("");
+  const [selectedRole, setSelectedRole] = useState("");
+  const [selectedManager, setSelectedManager] = useState("");
+
+  const handleAddEmployee = () => {
+    const newEmployee: Omit<Employee, "uid"> = {
+      name,
+      role: selectedRole,
+      manager: selectedManager,
+    };
+    addItem(newEmployee, "employees");
+    setName("");
+    setSelectedRole("");
+    setSelectedManager("");
+  };
+
+  return (
+    <div>
+      <div>
+        <label className={s.label}>
+          Name:
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+            className={s.input}
+          />
+        </label>
+      </div>
+      <div>
+        <label className={s.label}>
+          Role:
+          <div className={s.roleList}>
+            {roles.map((roleOption, index) => (
+              <div
+                key={index}
+                className={`${s.roleItem} ${selectedRole === roleOption ? s.selectedRole : ""}`}
+                onClick={() => setSelectedRole(roleOption)}
+              >
+                {roleOption}
+              </div>
+            ))}
+          </div>
+        </label>
+      </div>
+      <div>
+        <label className={s.label}>
+          Manager:
+          <div className={s.managerList}>
+            {managers.map((manager, index) => (
+              <div
+                key={index}
+                className={`${s.managerItem} ${selectedManager === manager ? s.selectedManager : ""}`}
+                onClick={() => setSelectedManager(manager)}
+              >
+                {manager}
+              </div>
+            ))}
+          </div>
+        </label>
+      </div>
+      <button
+        type="button"
+        className={s.button}
+        onClick={handleAddEmployee}
+        disabled={!name || !selectedRole || !selectedManager}
+      >
+        Add Employee
+      </button>
+    </div>
+  );
+};
+
+export default EmployeeMediator;
--- src/examples/behavioral/Mediator/MediatorEmployee.tsx
+++ src/examples/behavioral/Mediator/MediatorEmployee.tsx
@@ -1,85 +1,16 @@
-import { useState } from "react";
 import s from "./MediatorEmployee.module.scss";
-import { Employee, useStore } from "../store.ts";
+import { useStore } from "../store.ts";
 import { Card } from "../../../components/Card/Card.tsx";
-
-const managers = ["Alice", "Bob", "Charlie", "David", "Eve"];
-const roles = [
-  "Software Engineer",
-  "Product Manager",
-  "Data Scientist",
-  "UX Designer",
-  "QA Engineer",
-  "DevOps Engineer",
-  "Project Manager",
-  "Business Analyst",
-  "Marketing Specialist",
-  "HR Manager",
-];
+import Mediator from "./EmployeeMediator";
 
 const EmployeeList = () => {
   const { store, addItem } = useStore();
-  const [name, setName] = useState("");
-  const [role, setRole] = useState("");
-
-  const handleAddEmployee = () => {
-    const randomManager = managers[Math.floor(Math.random() * managers.length)];
-    const newEmployee: Omit<Employee, "uid"> = {
-      name,
-      role,
-      manager: randomManager,
-    };
-    addItem(newEmployee, "employees");
-    setName("");
-    setRole("");
-  };
-
   return (
     <Card>
+      <h1>Pattern Mediator</h1>
+      <p>Он же в экосистеме Реакт - "Container Component"</p>
       <div className={s.container}>
-        <form
-          className={s.form}
-          onSubmit={(e) => {
-            e.preventDefault();
-            handleAddEmployee();
-          }}
-        >
-          <div>
-            <label className={s.label}>
-              Name:
-              <input
-                type="text"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                required
-                className={s.input}
-              />
-            </label>
-          </div>
-          <div>
-            <label className={s.label}>
-              Role:
-              <select
-                value={role}
-                onChange={(e) => setRole(e.target.value)}
-                required
-                className={s.input}
-              >
-                <option value="" disabled>
-                  Select a role
-                </option>
-                {roles.map((roleOption, index) => (
-                  <option key={index} value={roleOption}>
-                    {roleOption}
-                  </option>
-                ))}
-              </select>
-            </label>
-          </div>
-          <button type="submit" className={s.button}>
-            Add Employee
-          </button>
-        </form>
+        <Mediator addItem={addItem} />
         <h2>Employees</h2>
         <ul className={s.employeeList}>
           {store.employees.map((employee) => (
@@ -88,14 +19,6 @@ const EmployeeList = () => {
             </li>
           ))}
         </ul>
-        <h2>Managers</h2>
-        <div className={s.managerList}>
-          {managers.map((manager, index) => (
-            <div key={index} className={s.managerItem}>
-              {manager}
-            </div>
-          ))}
-        </div>
       </div>
     </Card>
   );
