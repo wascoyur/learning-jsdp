fd72a175b0c9cd8f3cfcee2e213b84d4d56a9bd8
yuriy.vasilev <Yuriy.Va.Vasilev@ural.rt.ru>
2025-02-19 09:38:56 +0500

    DB init

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { DBinteraction } from "./storage/interactionDB.ts";
 
-createRoot(document.getElementById('root')!).render(
+const { initDB: initializeDB } = DBinteraction();
+
+initializeDB("globalStorage")
+  .then((r) => {
+    console.debug(`DB ${r} initialized`);
+  })
+  .catch((error) => {
+    console.error(`DB initialized Error ${error}`);
+  });
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
