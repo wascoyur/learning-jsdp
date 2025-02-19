a10a98cbcd02c00ce0de175b4fcc754b9c573283
yuriy.vasilev <Yuriy.Va.Vasilev@ural.rt.ru>
2025-02-19 18:24:00 +0500

    Revert "DB init"
    This reverts commit fd72a175b0c9cd8f3cfcee2e213b84d4d56a9bd8.

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
-import { DBinteraction } from "./storage/interactionDB.ts";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-const { initDB: initializeDB } = DBinteraction();
-
-initializeDB("globalStorage")
-  .then((r) => {
-    console.debug(`DB ${r} initialized`);
-  })
-  .catch((error) => {
-    console.error(`DB initialized Error ${error}`);
-  });
-
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-);
+)
