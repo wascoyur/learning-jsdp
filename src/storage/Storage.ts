b4ad833dfc3ba6614f548c26635a312d924556c1
yuriy.vasilev <Yuriy.Va.Vasilev@ural.rt.ru>
2025-02-20 10:14:49 +0500

    add blank new storage

--- src/storage/Storage.ts
+++ src/storage/Storage.ts
@@ -1,73 +1,174 @@
-import { useState, useEffect } from "react";
-import { v4 as uuidv4 } from "uuid";
+import { useCallback, useEffect, useState } from "react";
 
-export type Employee = {
-  uid: string;
-  name: string;
-  role: string;
-  manager: string;
+const Database = {
+  version: 1,
 };
 
-export type Topic = {
-  uid: string;
-  content: string;
-  identifier: string;
-  stockPrice: number;
-  date: number;
-};
+interface UseStorageResult {
+  getValue: (tableName: string, id: number) => Promise<any>;
+  getAllValue: (tableName: string) => Promise<any[]>;
+  putValue: (tableName: string, value: object) => Promise<IDBValidKey | null>;
+  putBulkValue: (tableName: string, values: object[]) => Promise<any[]>;
+  updateValue: (params: {
+    tableName: string;
+    id: number;
+    newItem: any;
+  }) => void;
+  deleteValue: (tableName: string, id: number) => number | null;
+  deleteAll: (tableName: string) => void;
+  isDBConnecting: boolean;
+}
 
-export type Storage = {
-  employees: Employee[];
-  topics: Topic[];
-};
-
-let globalStore: Storage = { employees: [], topics: [] };
-let subscribers: ((store: Storage) => void)[] = [];
-
-export const useStorage = () => {
-  const [store, setStore] = useState<Storage>(globalStore);
-
+export const useStorage = (
+  databaseName: string,
+  tableNames: string[],
+): UseStorageResult => {
+  const [db, setDB] = useState<IDBDatabase | null>(null);
+  const [isDBConnecting, setIsDBConnecting] = useState<boolean>(true);
   useEffect(() => {
-    const handleStoreUpdate = (updatedStore: Storage) => {
-      setStore(updatedStore);
+    const initDB = () => {
+      const request = indexedDB.open(databaseName, Database.version);
+      request.onupgradeneeded = () => {
+        const database = request.result;
+        tableNames.forEach((tableName) => {
+          if (!database.objectStoreNames.contains(tableName)) {
+            database.createObjectStore(tableName, {
+              autoIncrement: true,
+              keyPath: "id",
+            });
+          }
+        });
+      };
+
+      request.onsuccess = () => {
+        setDB(request.result);
+        setIsDBConnecting(false);
+      };
+
+      request.onerror = () => {
+        console.error("Error initializing IndexedDB:", request.error);
+        setIsDBConnecting(false);
+      };
     };
 
-    subscribers.push(handleStoreUpdate);
+    if (!db) {
+      initDB();
+    }
+  }, [databaseName, tableNames, db]);
 
-    return () => {
-      subscribers = subscribers.filter((sub) => sub !== handleStoreUpdate);
-    };
-  }, []);
-
-  const addItem = <T extends { uid: string }>(
-    item: Omit<T, "uid">,
-    key: keyof Storage,
-  ) => {
-    const newItem = { ...item, uid: uuidv4() };
-    globalStore = {
-      ...globalStore,
-      [key]: [...globalStore[key], newItem],
-    };
-    notifySubscribers();
+  const getTransaction = (tableName: string, mode: IDBTransactionMode) => {
+    if (!db) throw new Error("Database is not initialized");
+    return db.transaction(tableName, mode).objectStore(tableName);
   };
 
-  const initStore = () => {
-    globalStore = { employees: [], topics: [] };
-    setStore(globalStore);
-    notifySubscribers();
+  const getValue = useCallback(
+    (tableName: string, id: number): Promise<any> => {
+      return new Promise((resolve, reject) => {
+        try {
+          const store = getTransaction(tableName, "readonly");
+          const request = store.get(id);
+          request.onsuccess = () => resolve(request.result);
+          request.onerror = () => reject(request.error);
+        } catch (error) {
+          reject(error);
+        }
+      });
+    },
+    [db],
+  );
+
+  const getAllValue = (tableName: string): Promise<any[]> => {
+    return new Promise((resolve, reject) => {
+      try {
+        const store = getTransaction(tableName, "readonly");
+        const request = store.getAll();
+        request.onsuccess = () => resolve(request.result);
+        request.onerror = () => reject(request.error);
+      } catch (error) {
+        reject(error);
+      }
+    });
   };
 
-  const subscribe = (callback: (store: Storage) => void) => {
-    subscribers.push(callback);
+  const putValue = (
+    tableName: string,
+    value: object,
+  ): Promise<IDBValidKey | null> => {
+    return new Promise((resolve, reject) => {
+      try {
+        const store = getTransaction(tableName, "readwrite");
+        const request = store.put(value);
+        request.onsuccess = () => resolve(request.result);
+        request.onerror = () => reject(request.error);
+      } catch (error) {
+        reject(error);
+      }
+    });
   };
 
-  const unsubscribe = (callback: (store: Storage) => void) => {
-    subscribers = subscribers.filter((sub) => sub !== callback);
+  const putBulkValue = async (
+    tableName: string,
+    values: object[],
+  ): Promise<any[]> => {
+    try {
+      const store = getTransaction(tableName, "readwrite");
+      values.forEach((value) => store.put(value));
+      return getAllValue(tableName);
+    } catch (error) {
+      throw new Error("Bulk insert failed: " + error);
+    }
   };
 
-  const notifySubscribers = () => {
-    subscribers.forEach((callback) => callback(globalStore));
+  const updateValue = ({
+    tableName,
+    id,
+    newItem,
+  }: {
+    tableName: string;
+    id: number;
+    newItem: any;
+  }) => {
+    try {
+      const store = getTransaction(tableName, "readwrite");
+      const request = store.get(id);
+      request.onsuccess = () => {
+        const data = request.result;
+        const updatedItem = data ? { ...data, ...newItem } : { id, newItem };
+        store.put(updatedItem);
+      };
+    } catch (error) {
+      console.error("Update value failed: ", error);
+    }
   };
 
-  return { store, addItem, initStore, subscribe, unsubscribe };
+  const deleteValue = (tableName: string, id: number): number | null => {
+    try {
+      const store = getTransaction(tableName, "readwrite");
+      store.delete(id);
+      return id;
+    } catch (error) {
+      console.error("Delete value failed: ", error);
+      return null;
+    }
+  };
+
+  const deleteAll = (tableName: string) => {
+    try {
+      const store = getTransaction(tableName, "readwrite");
+      store.clear();
+    } catch (error) {
+      console.error("Delete all values failed: ", error);
+    }
+  };
+
+  return {
+    getValue,
+    getAllValue,
+    putValue,
+    putBulkValue,
+    updateValue,
+    deleteValue,
+    deleteAll,
+    isDBConnecting,
+  };
 };
